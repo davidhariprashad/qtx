390a726fa2894ede5684666831dc14452cc53c8a
DavidHariprashad <hariprashad.david@gmail.com>
2020-03-07 09:06:50 -0500

    switched function order

--- src/main/java/SliderTest.java
+++ src/main/java/SliderTest.java
@@ -8,13 +8,40 @@ import org.testng.annotations.Test;
 
 public class SliderTest extends SuperTestClass {
 
+	@Test
+	@Override
+	public void test() {
+
+		final SliderPageObject pageObject = new SliderPageObject(webDriver);
+		final WebElement slider = pageObject
+				.navigate("horizontal_slider")
+				.getRange();
+		final double minValue = Double.parseDouble(slider.getAttribute("min").trim());
+		final double maxValue = Double.parseDouble(slider.getAttribute("max").trim());
+		final Rectangle sliderRectangle = slider.getRect();
+
+		final Action dragSliderToMax = new Actions(webDriver)
+				.moveToElement(slider, 0, sliderRectangle.height/2)
+				.clickAndHold()
+				.moveByOffset(sliderRectangle.width, 0)
+				.release()
+				.build();
+
+		final double before = pageObject.getRangeValue();
+		Assert.assertEquals(before, minValue);
+		dragSliderToMax.perform();
+		final double after = pageObject.getRangeValue();
+		Assert.assertEquals(after, maxValue);
+
+	}
+
 	public void naivetest() {
-		
+
 		SliderPageObject pageObject = new SliderPageObject(webDriver);
 		WebElement slider = pageObject.
 				navigate("horizontal_slider")
 				.getRange();
-		
+
 		Action setSliderToMaxValue = new Actions(webDriver)
 				.moveToElement(slider)
 				.click()
@@ -28,7 +55,7 @@ public class SliderTest extends SuperTestClass {
 						Keys.ARROW_RIGHT,
 						Keys.ARROW_RIGHT)
 				.build();
-		
+
 		double before = pageObject.getRangeValue();
 		Assert.assertEquals(before, 0.0);
 		setSliderToMaxValue.perform();
@@ -36,34 +63,7 @@ public class SliderTest extends SuperTestClass {
 		System.out.println(new StringBuilder("Before: ").append(before).toString());
 		System.out.println(new StringBuilder("After: ").append(after).toString());
 		Assert.assertEquals(after, 5.0);
-		
-	}
-	
-	@Test
-	@Override
-	public void test() {
-		
-		final SliderPageObject pageObject = new SliderPageObject(webDriver);
-		final WebElement slider = pageObject
-				.navigate("horizontal_slider")
-				.getRange();
-		final double minValue = Double.parseDouble(slider.getAttribute("min").trim());
-		final double maxValue = Double.parseDouble(slider.getAttribute("max").trim());
-		final Rectangle sliderRectangle = slider.getRect();
-		
-		final Action dragSliderToMax = new Actions(webDriver)
-				.moveToElement(slider, 0, sliderRectangle.height/2)
-				.clickAndHold()
-				.moveByOffset(sliderRectangle.width, 0)
-				.release()
-				.build();
-		
-		final double before = pageObject.getRangeValue();
-		Assert.assertEquals(before, minValue);
-		dragSliderToMax.perform();
-		final double after = pageObject.getRangeValue();
-		Assert.assertEquals(after, maxValue);
-		
+
 	}
 
 }
